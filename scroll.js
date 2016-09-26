2694180238323818c6a4432e39cc1da22e9b3779
willashley23 <willashley23@gmail.com>
2016-09-25 18:39:10 -0700

    Refactor scroll js

--- scroll.js
+++ scroll.js
@@ -1,23 +1,11 @@
 $(document).ready(function() {
-    
     $(window).scroll( function(){
-        
         $('.hidden-project').each( function(i){
-            
             var bottom_of_object = $(this).offset().top + $(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
-            console.log(bottom_of_window)
-            console.log(bottom_of_object)
-           
-            if( bottom_of_window > bottom_of_object ){
-                console.log("happy")
-                $(this).slideDown(3000).animate(
-                  {'opacity':'1'}, 700);
-                 
+            if( bottom_of_window+200 > bottom_of_object ){
+                $(this).addClass("animated fadeInDown")
             }
-            
         }); 
-    
-    });
-    
+    }); 
 });
