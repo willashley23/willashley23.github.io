6b5f8cb279c45a018eac6ce4f416d1a96dec1e8f
willashley23 <willashley23@gmail.com>
2016-09-25 23:14:29 -0700

    Add edu and contact columns to webdev.

--- scroll.js
+++ scroll.js
@@ -3,9 +3,23 @@ $(document).ready(function() {
         $('.hidden-project').each( function(i){
             var bottom_of_object = $(this).offset().top + $(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
-            if( bottom_of_window+200 > bottom_of_object ){
+            if(bottom_of_window+200 > bottom_of_object) {
                 $(this).addClass("animated fadeInDown")
             }
         }); 
+        $('.hiddencol1').each( function(i) {
+          let bottomObject = $(this).offset().top + $(this).outerHeight();
+          let bottomWindow = $(window).scrollTop() + $(window).height();
+          if(bottomWindow+200 > bottomObject) {
+            $(this).addClass("animated fadeInLeft")
+          }
+        });
+        $('.hiddencol2').each( function(i) {
+          let bottomObject = $(this).offset().top + $(this).outerHeight();
+          let bottomWindow = $(window).scrollTop() + $(window).height();
+          if(bottomWindow+200 > bottomObject) {
+            $(this).addClass("animated fadeInRight")
+          }
+        });
     }); 
 });
